786adce2af64c50eb0a1d67eb1685f652f9cbcfd
BlessingNM <mpukutablessingnaison@gmail.com>
2024-05-15 14:26:46 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,25 @@
+//nav bar effects
+function myMenuFunction() {
+  var menuBtn = document.getElementById("myNavMenu");
+
+  if (menuBtn.className === "nav-menu") {
+    menuBtn.className += " responsive";
+  } else {
+    menuBtn.className = "nav-menu";
+  }
+}
+function myNavMonitor() {
+  if (window.scrollY >= 20) alert("meratedia");
+}
+/* 
+
+window.addEventListener("scroll", function () {
+  var element = document.getElementById("nimg");
+  var menuBtn = document.getElementsById("myNavMenu");
+  if (window.scrollY > 170 && menuBtn.className === "nav-menu responsive") {
+    element.style.display = "block";
+  } else {
+    element.style.display = "none";
+  }
+}); */
 
